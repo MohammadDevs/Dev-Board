0a34fac5224a959d5c34392110a48283ec81fc2a
MuhammadDevs <deenmuhammad19n@gmail.com>
2025-03-01 14:12:21 +0600

    added some section

---
+++ js/DevBoard.js
@@ -0,0 +1,24 @@
+
+// this is data storage. but I can't understand fully. 
+// why we use this , if we use any array or other kind of variabl, is it work?
+const storage = new Date();
+
+// দিনের নাম এবং তারিখ আলাদা আলাদা ফরম্যাটে
+const optionsDay = { weekday: 'short' };  // পূর্ণ দিনের নাম
+const dayName = tata.toLocaleDateString('en-US', optionsDay);  // দিনের নাম
+// দিন প্রদর্শন
+document.getElementById("current-day").innerText = dayName;
+
+
+const optionsDate = { year: 'numeric', month: 'short', day: 'numeric' };  // পূর্ণ তারিখ
+const fullDate = tata.toLocaleDateString('en-US', optionsDate);  // তারিখ
+// তারিখ প্রদর্শন
+document.getElementById("current-date").innerText = fullDate;
+
+document.getElementById('current-day').addEventListener(click, function(){
+    console.log('khela hobe')
+})
+    
+
+    
+
